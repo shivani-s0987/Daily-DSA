98869cbaae51a6b552f4c48d33fc11df29c5b14b
Anurag Revankar <shivanisakrappashivani@gmail.com>
2026-02-04 19:23:14 +0530

    first commit

--- medianarray.py
+++ medianarray.py
@@ -1,9 +1,9 @@
-class Slotion(object):
+class Solution(object):
     def findMedianSortedArrays(self,nums1,nums2):
-        merge=sorted(nums1+nums2)
-        n=len(merge)
-        mid=n//2
-        if n%2==1:
-            return merge[mid]
+        merged=sorted(nums1+nums2)
+        m=len(merged)
+        mid=m//2
+        if m%2==1:
+            return merged[mid]
         else:
-            return(merge[mid-1]+merge[mid])/2.0
+            return(merged[mid-1]+merged[mid])/2.0

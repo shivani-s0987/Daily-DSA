0d91c5eb6a4ddc744f02f781000803515662184d
Anurag Revankar <shivanisakrappashivani@gmail.com>
2026-02-23 18:06:58 +0530

    first commit

--- frequencymap.py
+++ frequencymap.py
@@ -1,9 +1,18 @@
-class Solution(onject):
+class Solution(object):
     def frequencyMap(self,nums):
-        frequence={} or dict()
+        frequence={} # dict()
         for i in range(0,len(nums)):
             if nums[i] in frequence:
                 frequence[nums[i]]+=1
             else:
                 frequence[nums[i]]=1
+        return frequence
+    
+#another method
+class Solution(object):
+    def frequencyMap(self,nums):
+        frequence={}
+        n=len(nums)
+        for i in range(0,n):
+            frequence[nums[i]]=frequence.get(nums[i],0)+1
         return frequence

0ef6ff5ce4191cc3e259a294b2b6ce0e46d02c04
Anurag Revankar <shivanisakrappashivani@gmail.com>
2026-03-05 19:07:48 +0530

    first commit

---
+++ Tc.py
@@ -0,0 +1,6 @@
+# time complexity :the rate of increse in time with respect to increse in the inputfor
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i,j)
+        # in above code tc is o(n^2) because we have two nested loops each running n times
+        

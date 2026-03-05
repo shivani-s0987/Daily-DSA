a594e7e75c607feda0f4dc5fe17778630e35dab8
Anurag Revankar <shivanisakrappashivani@gmail.com>
2026-03-05 19:10:24 +0530

    first commit

--- Tc.py
+++ Tc.py
@@ -3,4 +3,8 @@ for i in range(1,n+1):
     for j in range(1,n+1):
         print(i,j)
         # in above code tc is o(n^2) because we have two nested loops each running n times
-        
+
+for i in range(1,n+1):
+    for j in range(i,n+1):
+        print(i,j)
+        #in naturally it is n(n+1)/2 but in big o notation we ignore the constant and lower order term so it is o(n^2)

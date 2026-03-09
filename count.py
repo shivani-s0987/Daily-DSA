e38d1e4fa2dc2461c92fcd3bb0af72be546ddb78
Anurag Revankar <shivanisakrappashivani@gmail.com>
2026-03-09 19:29:58 +0530

    first commit

--- count.py
+++ count.py
@@ -5,4 +5,14 @@ while num>0:
     count=count+1
     num=num//10
 print(count)
-    
+
+#another way
+class Solution(object):
+    def countDigits(self,n):
+        count=0
+        for i in str(n):
+            if n%int(i)==0:
+                count+=1
+            return count
+
+
